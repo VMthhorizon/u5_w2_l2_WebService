9ca8a8a8b1451018ad5c8dc21550e397bd490fec
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-07-07 14:38:34 +0200

    Initial commit

---
+++ src/main/java/vincenzomola/u5_w2_l2_WebService/U5W2L2WebServiceApplication.java
@@ -0,0 +1,13 @@
+package vincenzomola.u5_w2_l2_WebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W2L2WebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W2L2WebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vincenzomola/u5_w2_l2_WebService/U5W2L2WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vincenzomola.u5_w2_l2_WebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W2L2WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
